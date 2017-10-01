adf5c241498e3c7a48d528e4730f2dac3e53ff10
Fabian Buentello <fbuentelloapp@gmail.com>
2017-10-01 12:26:33 -0500

    init commit Hello world

---
+++ src/components/base.layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Base = (props) => (
+    <div>
+        <h1>Hello</h1>
+    </div>
+)
+export default Base
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: './src/app.jsx',
+    output: {
+        filename: 'app.bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        inline: true,
+        contentBase: './dist',
+        port: 3000
+    },
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'node_modules')
+        ],
+        extensions: ['.js', '.jsx', '.json']
+    },
+    devtool: 'inline-source-map',
+    module: {
+        loaders: [
+            { test: /\.html$/, loader: 'file-loader?name=[name].[ext]' },
+            { test: /\.jsx$/, exclude: /node_modules/, loaders: ['react-hot-loader', 'babel-loader'] }
+        ]
+    }
+}
